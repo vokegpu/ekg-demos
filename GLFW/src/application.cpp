711ffdb45369913e18b7350ff6cd393a4472a4ca
FabricioLR <fabriciolongodearaujo@gmail.com>
2025-04-06 07:59:48 -0300

    refactor: rewrite glfw and sdl examples

--- GLFW/src/application.cpp
+++ GLFW/src/application.cpp
@@ -1,31 +1,32 @@
-#include <iostream>
-
 #include <ekg/os/ekg_opengl.hpp>
 #include <ekg/os/ekg_glfw.hpp>
 #include <ekg/ekg.hpp>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 
 static void window_size_callback(GLFWwindow* p_glfw_win, int width, int height) {
-  ekg::os::glfw_window_size_callback(width, height);
+  ekg::glfw_window_size_callback(width, height);
 }
 
 static void key_callback(GLFWwindow* p_glfw_win, int key, int scancode, int action, int mods) {
-  ekg::os::glfw_key_callback(key, scancode, action, mods);
+  ekg::glfw_key_callback(key, scancode, action, mods);
 }
 
 static void character_callback(GLFWwindow* p_glfw_win, unsigned int codepoint) {
-  ekg::os::glfw_char_callback(codepoint);
+  ekg::glfw_char_callback(codepoint);
 }
 
 static void cursor_position_callback(GLFWwindow* p_glfw_win, double x, double y) {
-  ekg::os::glfw_cursor_pos_callback(x, y);
+  ekg::glfw_cursor_pos_callback(x, y);
 }
 
 static void mouse_button_callback(GLFWwindow* p_glfw_win, int button, int action, int mods) {
-  ekg::os::glfw_mouse_button_callback(button, action, mods);
+  ekg::glfw_mouse_button_callback(button, action, mods);
 }
 
 static void scroll_callback(GLFWwindow* p_glfw_win, double dx, double dy) {
-  ekg::os::glfw_scroll_callback(dx, dy);
+  ekg::glfw_scroll_callback(dx, dy);
 }
 
 int32_t main(int32_t, char**) {
@@ -35,7 +36,7 @@ int32_t main(int32_t, char**) {
 
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
   GLFWwindow *p_glfw_win {
     glfwCreateWindow(
@@ -52,11 +53,11 @@ int32_t main(int32_t, char**) {
   glewExperimental = GL_TRUE;
   glewInit();
 
-  ekg::runtime_property ekg_property {
-    .p_font_path = "./whitneybook.otf",
-    .p_font_path_emoji = "./openmojicolor.ttf",
-    .p_gpu_api = new ekg::os::opengl(),
-    .p_os_platform = new ekg::os::glfw(p_glfw_win)
+  ekg::runtime_property_t ekg_property {
+    .font_path = "./whitneybook.otf",
+    .font_path_emoji = "./openmojicolor.ttf",
+    .p_gpu_api = new ekg::opengl("#version 400"),
+    .p_os_platform = new ekg::glfw(p_glfw_win)
   };
 
   ekg::runtime ekg_runtime {};
@@ -70,34 +71,48 @@ int32_t main(int32_t, char**) {
   glfwSetWindowSizeCallback(p_glfw_win, window_size_callback);
   glfwSwapInterval(1);
 
-  ekg::frame("meow oi amo vc", {20, 20}, {400, 200})
-    ->set_drag(ekg::dock::top)
-    ->set_resize(ekg::dock::left | ekg::dock::bottom | ekg::dock::right);
+  ekg::make(
+    ekg::frame_t {
+      .tag = "first frame",
+      .rect = {00.0f, 00.0f, 400.0f, 400.0f},
+      .drag_dock = ekg::dock::top,
+      .resize_dock = ekg::dock::left | ekg::dock::bottom | ekg::dock::right
+    }
+  );
 
-  ekg::label("working on GLFW3", ekg::dock::fill | ekg::dock::next);
-  ekg::label("very cool...", ekg::dock::fill | ekg::dock::next);
-  ekg::label("meow", ekg::dock::fill | ekg::dock::next);
-  ekg::button("testing button because we", ekg::dock::fill | ekg::dock::next);
+  ekg::button_t button {};
 
-  ekg::textbox("meow1", "oi eu amo gato frajola fofa perfeita eu quero casar", ekg::dock::fill | ekg::dock::next)
-    ->set_scaled_height(4);
+  button.tag = "bt-1";
+  button.text = "moo moo";
+  button.text_dock = ekg::dock::left;
+  button.dock = ekg::dock::left;
+  button.text_font_size = ekg::font::big;
+  ekg::make(button);
 
-  ekg::pop_group();
+  button.tag = "bt-2";
+  button.text = "owlf olwf";
+  button.text_dock = ekg::dock::center;
+  button.dock = ekg::dock::next | ekg::dock::fill;
+  button.text_font_size = ekg::font::big;
+  ekg::make(button);
 
-  ekg::input::bind("amovc", "a");
+  button.tag = "bt-3";
+  button.text = "oi amo gatinhos";
+  button.text_font_size = ekg::font::big;
+  ekg::make(button);
 
   while (!glfwWindowShouldClose(p_glfw_win)) {
+    //Must be before update function
+    if (ekg::input("mouse-1")) {
+      std::cout << "Eu amo muito vc <3" << std::endl;
+    }
+
     ekg::update();
 
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.143243f, 0.2f, 0.921721823f, 1.0f);
-    glViewport(0.0f, 0.0f, ekg::ui::width, ekg::ui::height);
+    glViewport(0.0f, 0.0f, ekg::viewport.w, ekg::viewport.h);
 
-    if (ekg::input::action("amovc")) {
-      std::cout << "Eu amo muito vc <3" << std::endl;
-    }
-
-    ekg::ui::dt = 0.016f;
     ekg::render();
 
     glfwSwapBuffers(p_glfw_win);
